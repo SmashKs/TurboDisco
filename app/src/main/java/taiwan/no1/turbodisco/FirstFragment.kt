b0710e5794a1c994116674deedbf02eba3b6c710
Jieyi <pokkbaby@gmail.com>
2023-01-13 00:43:30 +0900

    feat(*): add firebase dependencies

--- app/src/main/java/taiwan/no1/turbodisco/FirstFragment.kt
+++ app/src/main/java/taiwan/no1/turbodisco/FirstFragment.kt
@@ -12,7 +12,6 @@ import taiwan.no1.turbodisco.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
