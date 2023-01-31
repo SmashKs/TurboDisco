e3a5b7383b312da4c55e8a148e807f66042d5d0f
Jieyi <pokkbaby@gmail.com>
2023-01-31 23:05:43 +0900

    build(*): refactor to the gralde kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    namespace = "taiwan.no1.turbodisco"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "taiwan.no1.turbodisco"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), file("proguard-rules.pro"))
+        }
+    }
+
+    packagingOptions {
+        resources.excludes += "DebugProbesKt.bin"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        suppressWarnings = false
+        languageVersion = "1.9"
+        freeCompilerArgs = listOf("-Xcontext-receivers")
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    //region Jetpack Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.activity.compose)
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.runtime.livedata)
+    implementation(libs.coil.compose)
+
+    debugImplementation(libs.debug.ui.tooling)
+    //endregion
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.startup.runtime)
+    implementation(libs.work.runtime.ktx)
+
+    implementation(libs.datastore.preferences)
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
+    testImplementation(libs.junit.junit)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
