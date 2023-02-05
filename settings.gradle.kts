d1cfef2c0b1f334248362809ed57def13e326149
Jieyi <pokkbaby@gmail.com>
2023-02-06 01:52:07 +0900

    build(*): add necessary modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,8 @@ pluginManagement {
     }
     plugins {
         id("de.fayard.refreshVersions").version("0.51.0")
+        id("com.android.library") version "8.1.0-alpha02"
+        id("org.jetbrains.kotlin.android") version "1.8.0"
     }
 }
 
@@ -23,3 +25,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "TurboDisco"
 include(":app")
+include(":feat:review")
+include(":feat:profile")
+include(":feat:auth")
+include(":feat:comment")
+include(":feat:sake")
+include(":feat:search")
+include(":lib:analytic")
+include(":lib:ktx")
+include(":lib:core")
