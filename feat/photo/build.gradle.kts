cc643927f8dfe4bfb9ad75229cd95ef9ee447366
Jieyi <pokkbaby@gmail.com>
2024-01-31 01:36:23 +0900

    feat: add Jetpack Compose dependencies for favorite, photo, profile, and search screens
    Added Jetpack Compose dependencies for the favorite, photo, profile, and search screens in the build.gradle.kts file.

--- feat/photo/build.gradle.kts
+++ feat/photo/build.gradle.kts
@@ -4,6 +4,20 @@ android {
 
 dependencies {
     implementation(libs.core.ktx)
+
+    //region Jetpack Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.runtime.livedata)
+    implementation(libs.coil.compose)
+
+    implementation(libs.accompanist.permissions)
+
+    debugImplementation(libs.debug.ui.tooling)
+    //endregion
+
     testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ feat/profile/src/main/kotlin/taiwan/no/one/profile/ProfileScreen.kt
@@ -0,0 +1,37 @@
+package taiwan.no.one.profile
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Profile Screen") })
+        },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = modifier.padding(paddingValues),
+        ) {
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSearchScreen() {
+    MaterialTheme {
+        ProfileScreen()
+    }
+}
